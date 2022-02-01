8a5b32dd9d2f7152cd8cfd59514799491a85a88b
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-02-01 22:04:40 +0600

    variables

---
+++ variable-types.js
@@ -0,0 +1,27 @@
+// Number--integer
+let studentId = 181311002;
+let age = 21;
+let length = 25;
+
+//Number--Float
+let weight = 65.5;
+let something = 4.15451;
+
+// String
+let name = "Arifuzzaman Tusar";
+let address = "Bangladesh";
+
+// Boolean
+let stats = true;
+let stats = false;
+
+// Array
+let cars = ["Saab", "Volvo", "BMW"];
+
+// Object
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
