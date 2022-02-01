c0447c08eb9c91c228a6bc513eef1c4805e4ebff
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-02-01 21:25:14 +0600

    array push

---
+++ array-push-pop.js
@@ -0,0 +1,9 @@
+var array= ['kalam','balam','salam','jabbar'];
+
+// Pushung an elements into that array 
+array.push('tusar');
+//Popping out an array 
+array.pop('tusar');
+
+// ----
+console.log(array);
