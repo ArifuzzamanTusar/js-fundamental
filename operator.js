93e06a820a1366b5295f9126c9f2c250a23cf5e1
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-02-01 22:22:09 +0600

    typeof

---
+++ operator.js
@@ -0,0 +1,13 @@
+// variables 
+let numberOne = 20;
+let numberTwo = 5;
+
+let text = "TheText";
+
+
+
+// Operator-TypeOF 
+console.log(typeof numberOne);
+console.log(typeof text);
+// -- note: typeof operator returns the type of the veriable 
+
