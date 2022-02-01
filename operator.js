5188779f47b15b02654b4a7aa5277dd1a2128642
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-02-01 22:32:49 +0600

    operators

--- operator.js
+++ operator.js
@@ -9,5 +9,32 @@ let text = "TheText";
 // Operator-TypeOF 
 console.log(typeof numberOne);
 console.log(typeof text);
-// -- note: typeof operator returns the type of the veriable 
+// -- note: typeof operator returns the type of the variable 
 
+// ---------------------- Common Operators -----------------------
+// Operator--Assignment
+let numberOne = 25;
+// -- note: assigns value for a variable 
+
+// Operator--Add (+)
+let totalNumber= numberOne+numberTwo;
+
+// Operator--Substraction (-)
+let SubstructedNumber= numberOne-numberTwo;
+
+
+// Operator--Multiply (*)
+let multipliedNumber= numberOne*numberTwo;
+
+// Operator--Division (/)
+let devidedNumber= numberOne/numberTwo;
+
+//Operator--Modulus (Division Remainder) (%)
+let reminder= numberOne%numberTwo;
+
+
+//Operator--Increment (++)
+let Increment = numberOne++;
+
+//Operator--Decrement (--)
+let Decrement= numberOne--;
